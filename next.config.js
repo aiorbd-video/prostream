5713f6a43811d168958d387ab059f42ae948d30b
aiorbd-video <ratulhasan5a@gmail.com>
2026-02-05 23:07:15 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['via.placeholder.com', 'lh3.googleusercontent.com', 'encrypted-tbn0.gstatic.com'], // Add others if needed
+  },
+};
+
+module.exports = nextConfig;
