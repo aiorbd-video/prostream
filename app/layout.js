ce1b037e1e182bf84b9c4386a991ce42ee6e97ad
aiorbd-video <ratulhasan5a@gmail.com>
2026-02-07 10:37:49 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "StreamHub BD",
-  description: "Live Sports & IPTV",
+  title: "Ratul Liv",
+  description: "Live Sports & IPTV ",
 };
 
 export default function RootLayout({ children }) {
